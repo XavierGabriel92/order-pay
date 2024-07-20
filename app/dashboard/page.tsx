7f9f7ac967c72be7f39ab428afb2b902d7929afa
gabriel.xavier <xaviergabriel92@gmail.com>
2024-07-20 11:14:56 -0300

    first commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Dashboard() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>Meu app</div>
+			<div>Dashboard</div>
 		</main>
 	);
 }
